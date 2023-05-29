c397f1233c23bde38611a5e382480a709fb08f59
Sangin <ksiisk99@gmail.com>
2023-05-29 23:51:48 +0900

    test: 티스토리 액세스 토큰 추출 테스트

---
+++ src/test/java/assignment/capstone/tistory/AccessTokenParserTest.java
@@ -0,0 +1,22 @@
+package assignment.capstone.tistory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccessTokenParserTest {
+
+    @Test
+    void getAccessToken() {
+        AccessTokenParser parser = new AccessTokenParser();
+
+        ClientInfo clientInfo = new ClientInfo("clientId",
+                "clientSecret",
+                "redirectUri",
+                "code");
+
+        assertDoesNotThrow(()->{
+            parser.getAccessToken(clientInfo);
+        });
+    }
+}
