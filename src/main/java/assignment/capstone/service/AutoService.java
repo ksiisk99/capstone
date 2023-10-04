2a812167af3234597bbeac787e9924cde973edc8
Sangin <ksiisk99@gmail.com>
2023-10-05 03:09:09 +0900

    feat: 이미 답글한 댓글은 제거

--- src/main/java/assignment/capstone/service/AutoService.java
+++ src/main/java/assignment/capstone/service/AutoService.java
@@ -1,6 +1,5 @@
 package assignment.capstone.service;
 
-import assignment.capstone.dto.BlogInfo;
 import assignment.capstone.dto.Comment;
 import assignment.capstone.entity.Blog;
 import assignment.capstone.service.gpt.ChatGptHandler;
@@ -9,8 +8,8 @@ import assignment.capstone.service.tistory.CommentWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,8 +26,6 @@ public class AutoService {
         List<String> commentResponses = chatGptHandler.createCommentResponses(comments);
 
         for (int i = 0; i < comments.size(); i++) {
-//            System.out.println("Comment: " + comments.get(i).getContent() + " " + comments.get(i).getParentId());
-//            System.out.println("ChatGpt: " + commentResponses.get(i));
             commentWriter.write(blog, comments.get(i).getId(), commentResponses.get(i), comments.get(i).isOpen());
         }
     }
@@ -38,18 +35,41 @@ public class AutoService {
 
         for (int i = 0; i < comments.size(); i++) {
             Comment comment = comments.get(i);
-            if (isParentComment(comment.getParentId())
-                    //|| alreadyCompleted(comment, homePage)
+            if (!isParentComment(comment.getParentId())
+                    || isOwnComment(comment, homePage)
             ) {
                 removeComments.add(comment);
             }
         }
-
         removeComments.stream()
                 .forEach(comment -> comments.remove(comment));
+
+        Set<String> alreadyCommentIds = getAlreadyCommentIds(comments, homePage);
+        Iterator<Comment> iterator = comments.iterator();
+        while (iterator.hasNext()) { //이미 답글한 댓글은 제거
+            Comment comment = iterator.next();
+            if (alreadyCommentIds.contains(comment.getId())) {
+                iterator.remove();
+            }
+        }
+
     }
 
-    private boolean alreadyCompleted(Comment comment, String homePage) {
+    private Set<String> getAlreadyCommentIds(List<Comment> comments, String homePage) {
+        return comments.stream()
+                .filter(c -> !isParentComment(c.getParentId()) && isAlreadyResponse(c.getHomepage(), homePage))
+                .map(c -> c.getParentId())
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    private boolean isAlreadyResponse(String s, String s2) {
+        if (s.equals(s2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isOwnComment(Comment comment, String homePage) {
         if (comment.getHomepage().equals(homePage)) {
             return true;
         }
